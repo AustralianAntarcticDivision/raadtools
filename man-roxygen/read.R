a3888a70d698ae996925b1549d8b324b9571f75a
Mike Sumner <michael.sumner@aad.gov.au>
2013-08-12 14:36:35 +1000

    add files

---
+++ man-roxygen/read.R
@@ -0,0 +1,7 @@
+#' @param date date-time specification
+#' @param time.resolution temporal resolution to read
+#' @param datadir path to repository of RAAD data
+#' @keywords manip
+#' @references SOKI Wiki (2013). Get started with toolbox - Data -
+#' Confluence, SOKI, Antarctic Climate and Ecosystems Co-operative
+#' Research Centre \url{http://soki.aad.gov.au/display/Data/Get+started+with+toolbox}

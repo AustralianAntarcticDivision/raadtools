2e9127c63cb2707df33b78e58ebb8d3b9bb950d4
Mike Sumner <michael.sumner@aad.gov.au>
2013-09-19 11:33:27 +1000

    first stab at John chla

--- R/raadtools.R
+++ R/raadtools.R
@@ -39,6 +39,91 @@ NULL
     }
 }
 
+
+
+##' Chlorophyll-a for the Southern Ocean 
+##' @export
+readchla <- function(date = as.Date("1997-08-291"), time.resolution = c("monthly", "weekly"),                 
+                    xylim = NULL,
+                    ##lon180 = TRUE,
+                    returnfiles = FALSE,
+                    verbose = TRUE,
+                    ...) {
+  
+  time.resolution <- match.arg(time.resolution)
+ 
+  files <- chlafiles()
+  if (returnfiles) return(files)
+  
+  ## from this point one, we don't care about the input "date" - this is our index into all files and that's what we use
+  findex <- .processDates(date, files$date, time.resolution)
+  date <- files$date[findex]
+  
+  rtemplate <- raster(files$fullname[findex[1]])
+  ##if (lon180) rtemplate <- rotate(rtemplate)
+  
+  ## process xylim
+  cropit <- FALSE
+  if (!is.null(xylim)) {
+    cropit <- TRUE
+    cropext <- extent(xylim)
+    ##rtemplate <- crop(rtemplate, cropext)
+  }
+  
+  nfiles <- length(findex)
+  r <- vector("list", nfiles)
+  
+  for (ifile in seq_len(nfiles)) {
+    r0 <- raster(files$fullname[findex[ifile]])
+    ##if (lon180) r0 <- rotate(r0)
+    if(cropit) r0 <- crop(r0, cropext)
+    ## r0[r0 < -2] <- NA
+    r[[ifile]] <- r0
+    ##if (verbose & ifile %% 10L == 0L) .progressreport(ifile, nfiles)
+  }
+  
+  if (nfiles > 1) 
+    r <- brick(stack(r))
+  else r <- r[[1L]]
+  names(r) <- basename(files$file[findex])
+  r <- setZ(r, files$date[findex])
+  return(r)
+}
+
+##' Chlorophyll-a for the Southern Ocean 
+##' 
+##' This function generates a list of available chlorophyll-a files, including SeaWiFS and MODIS. 
+##' @title Chlorophyll-a
+##' @param time.resolution monthly or weekly (8day)
+##' @return data.frame
+##' @export
+chlafiles <- function(time.resolution = c("monthly", "weekly")) {
+  data.dir <- getOption("default.datadir")
+  time.resolution <- match.arg(time.resolution)
+  fromCache <- TRUE
+  if (fromCache) {
+    load(file.path(data.dir, "cache", sprintf("%s_chlafiles.Rdata", time.resolution)))
+    chlf$fullname <- file.path(data.dir,  chlf$file)
+    return(chlf)
+  }
+  
+  dirpath <- file.path("chl", "johnson")
+  
+  tr <- c(monthly = "Monthly", weekly = "8day")[time.resolution]
+  fs <- c(file.path(dirpath, sprintf("MODISA_%s", tr), list.files(file.path(data.dir, dirpath, sprintf("MODISA_%s", tr)))), 
+          file.path(dirpath, sprintf("SeaWiFS_%s", tr), list.files(file.path(data.dir, dirpath, sprintf("SeaWiFS_%s", tr)))))
+     
+  dates <- timedateFrom(strptime(substr(basename(fs), 2, 8), "%Y%j"))
+  chlf <- data.frame(file = fs, date = dates, 
+                     stringsAsFactors = FALSE)[order(dates), ]
+                                                                          
+  save(chlf, file = file.path(data.dir, "cache", sprintf("%s_chlafiles.Rdata", time.resolution)))
+  chlf
+  
+  
+}
+  
+
 ##' Coastline data set as SpatialPolygons*
 ##'
 ##' This function reads and returns a coastline polygon data set, either from the

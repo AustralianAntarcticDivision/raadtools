e8f5d06460a73b6c7fa749b4f998c2b7b2c9fdfa
Mike Sumner <michael.sumner@aad.gov.au>
2013-10-01 16:24:07 +1000

    ocean colours

--- R/raadtools.R
+++ R/raadtools.R
@@ -39,6 +39,98 @@ NULL
     }
 }
 
+##' Ocean colour palette for chlorophyll-a.
+##'
+##' Flexible control of the chlorophyll-a palette. If \code{x} is a
+##' single number, the function returns that many colours evenly
+##' spaced from the palette. If \code{x} is a vector of multiple
+##' values the palette is queried for colours matching those values,
+##' and these are returned. If \code{x} is missing and \code{palette}
+##' is \code{FALSE} then a function is returned that will generate n
+##' evenly spaced colours from the palette, as per
+##' \code{\link{colorRampPalette}}.
+##' @title Ocean colour colours for chlorophyll-a.
+##' @param x a vector of data values or a single number
+##' @param palette logical, if \code{TRUE} return a list with matching colours and values
+##' @return colours, palette, or function, see Details
+##' @export
+##' @examples
+##' \dontrun{
+##' chl <- readchla(xylim = c(100, 110, -50, -40))
+##' ## just get a small number of evenly space colours
+##' plot(chl, col = chl.pal(10))
+##' ## store the full palette and work with values and colours
+##' pal <- chl.pal()
+##' ## the standard full palette
+##' plot(chl, breaks = pal$breaks, col = pal$cols)
+##' ## a custom set of values with matching colours
+##' plot(chl, col = chl.pal(pal$breaks[seq(1, length(pal$breaks), length = 10)]))
+##' ## any number of colours stored as a function
+##' myfun <- chl.pal()
+##' plot(chl, col = myfun(18))
+##' ## just n colours
+##' plot(chl, col = chl.pal(18))
+##' }
+chl.pal <- function(x, palette = FALSE) {
+
+    ##pal <- read.table("http://oceancolor.gsfc.nasa.gov/DOCS/palette_chl_etc.txt", skip = 1)
+    ##cols <- rgb(pal[,1], pal[,2], pal[,3], maxColorValue = 255)
+    ## dput(cols)
+    breaks <-  c(0, exp(round(seq(-4.6, 4.1, length = 255), digits = 2)))
+    cols <- c("#000000", "#90006F", "#8D0072", "#8A0075", "#870078", "#84007B",
+"#81007E", "#7E0081", "#7B0084", "#780087", "#75008A", "#72008D",
+"#6F0090", "#6C0093", "#690096", "#660099", "#63009C", "#60009F",
+"#5D00A2", "#5A00A5", "#5700A8", "#5400AB", "#5100AE", "#4E00B1",
+"#4B00B4", "#4800B7", "#4500BA", "#4200BD", "#3F00C0", "#3C00C3",
+"#3900C6", "#3600C9", "#3300CC", "#3000CF", "#2D00D2", "#2A00D5",
+"#2700D8", "#2400DB", "#2100DE", "#1E00E1", "#1B00E4", "#1800E7",
+"#1500EA", "#1200ED", "#0F00F0", "#0C00F3", "#0900F6", "#0600F9",
+"#0000FC", "#0000FF", "#0005FF", "#000AFF", "#0010FF", "#0015FF",
+"#001AFF", "#0020FF", "#0025FF", "#002AFF", "#0030FF", "#0035FF",
+"#003AFF", "#0040FF", "#0045FF", "#004AFF", "#0050FF", "#0055FF",
+"#005AFF", "#0060FF", "#0065FF", "#006AFF", "#0070FF", "#0075FF",
+"#007AFF", "#0080FF", "#0085FF", "#008AFF", "#0090FF", "#0095FF",
+"#009AFF", "#00A0FF", "#00A5FF", "#00AAFF", "#00B0FF", "#00B5FF",
+"#00BAFF", "#00C0FF", "#00C5FF", "#00CAFF", "#00D0FF", "#00D5FF",
+"#00DAFF", "#00E0FF", "#00E5FF", "#00EAFF", "#00F0FF", "#00F5FF",
+"#00FAFF", "#00FFFF", "#00FFF7", "#00FFEF", "#00FFE7", "#00FFDF",
+"#00FFD7", "#00FFCF", "#00FFC7", "#00FFBF", "#00FFB7", "#00FFAF",
+"#00FFA7", "#00FF9F", "#00FF97", "#00FF8F", "#00FF87", "#00FF7F",
+"#00FF77", "#00FF6F", "#00FF67", "#00FF5F", "#00FF57", "#00FF4F",
+"#00FF47", "#00FF3F", "#00FF37", "#00FF2F", "#00FF27", "#00FF1F",
+"#00FF17", "#00FF0F", "#00FF00", "#08FF00", "#10FF00", "#18FF00",
+"#20FF00", "#28FF00", "#30FF00", "#38FF00", "#40FF00", "#48FF00",
+"#50FF00", "#58FF00", "#60FF00", "#68FF00", "#70FF00", "#78FF00",
+"#80FF00", "#88FF00", "#90FF00", "#98FF00", "#A0FF00", "#A8FF00",
+"#B0FF00", "#B8FF00", "#C0FF00", "#C8FF00", "#D0FF00", "#D8FF00",
+"#E0FF00", "#E8FF00", "#F0FF00", "#F8FF00", "#FFFF00", "#FFFB00",
+"#FFF700", "#FFF300", "#FFEF00", "#FFEB00", "#FFE700", "#FFE300",
+"#FFDF00", "#FFDB00", "#FFD700", "#FFD300", "#FFCF00", "#FFCB00",
+"#FFC700", "#FFC300", "#FFBF00", "#FFBB00", "#FFB700", "#FFB300",
+"#FFAF00", "#FFAB00", "#FFA700", "#FFA300", "#FF9F00", "#FF9B00",
+"#FF9700", "#FF9300", "#FF8F00", "#FF8B00", "#FF8700", "#FF8300",
+"#FF7F00", "#FF7B00", "#FF7700", "#FF7300", "#FF6F00", "#FF6B00",
+"#FF6700", "#FF6300", "#FF5F00", "#FF5B00", "#FF5700", "#FF5300",
+"#FF4F00", "#FF4B00", "#FF4700", "#FF4300", "#FF3F00", "#FF3B00",
+"#FF3700", "#FF3300", "#FF2F00", "#FF2B00", "#FF2700", "#FF2300",
+"#FF1F00", "#FF1B00", "#FF1700", "#FF1300", "#FF0F00", "#FF0B00",
+"#FF0700", "#FF0300", "#FF0000", "#FA0000", "#F50000", "#F00000",
+"#EB0000", "#E60000", "#E10000", "#DC0000", "#D70000", "#D20000",
+"#CD0000", "#C80000", "#C30000", "#BE0000", "#B90000", "#B40000",
+"#AF0000", "#AA0000", "#A50000", "#A00000", "#9B0000", "#960000",
+"#910000", "#8C0000", "#870000", "#820000", "#7D0000", "#780000",
+"#730000", "#6E0000", "#690000", "#640000")
+
+    if (palette) return(list(breaks = breaks, cols = cols))
+    if (missing(x)) return(colorRampPalette(cols))
+
+    if (length(x) == 1L) {
+        return(colorRampPalette(cols)(x))
+    } else {
+        return(cols[findInterval(x, breaks)])
+    }
+
+}
 
 
 ##' Read Chlorophyll-a for the Southern Ocean
@@ -155,9 +247,11 @@ chlafiles <- function(time.resolution = c("monthly", "weekly")) {
 ##'
 ##' Currently ORSI is the only supported layer.
 ##'
-##' "orsi" - the ORSI fronts derived from the files
+##' "orsi" - the ORSI fronts derived from the files provided by the
+##' WOCE Atlas, see References
 ##' @title Fronts map data for the Southern Ocean
-##' @param map
+##' @param map name of map to load
+##' @references \url{http://woceatlas.tamu.edu/Sites/html/atlas/SOA_DATABASE_DOWNLOAD.html}
 ##' @return SpatialLinesDataFrame
 ##' @export
 frontsmap <- function(map = c("orsi")) {
@@ -265,8 +359,10 @@ coastmap <- function(map = c(
 function(world1 = TRUE) {
 ##    require(maptools)
 
-    wrld_simpl <- NULL
-   data("wrld_simpl", package = "maptools", envir = environment())
+##    wrld_simpl <- NULL
+    load(system.file("data", "wrld_simpl.rda", package = "maptools"))
+
+##   data("wrld_simpl", package = "maptools", envir = environment())
     if (world1) return(as(wrld_simpl, "SpatialPolygons"))
     ##require(raster)
    ## require(rgeos)

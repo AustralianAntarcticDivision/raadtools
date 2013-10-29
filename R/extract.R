421d47190907ce52228471b9ddc92aa992ef9652
Mike Sumner <michael.sumner@aad.gov.au>
2013-10-30 10:32:06 +1100

    document methods

--- R/extract.R
+++ R/extract.R
@@ -5,16 +5,35 @@
           }
 
 
-## method takes a function that reads multiple time slices
-##' @import methods
+
+##############################################################
+#' Extract methods for raadtools read functions
+#'
+#' Extract data from read functions in various ways.
+#'
+#' @param x A raadtools read function.
+#'
+#' @param y One of various means of querying from the raadtools read
+#' functions, such as a vector of character, Date, or POSIXt values,
+#' data.frame, trip, etc.
+#'
+#' @param ... Additional arguments passed to the read function.
+#'
+#' @return data values extracted by the read functions
+#'
+#' @seealso \code{\link{readsst}} and \code{\link{extract}}
+#'
+#' @export
+#' @docType methods
+#' @rdname raadtools-extract
+#' @import methods
+#' @exportMethod extract
 setMethod("extract", signature(x = 'function', y = 'POSIXt'), .read.generic)
+##' @exportMethod extract
 setMethod("extract", signature(x = 'function', y = 'Date'), .read.generic)
+##' @exportMethod extract
 setMethod("extract", signature(x = 'function', y = 'character'), .read.generic)
-
-
-
-
-##' @import methods
+##' @exportMethod extract
 setMethod("extract", signature(x = 'function', y = 'data.frame'),
           function(x, y, ...) {
               ## y better be x,y,t and in the right projection

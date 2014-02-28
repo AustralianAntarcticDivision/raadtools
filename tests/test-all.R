412bafa97630cf2d2b791ef2d87340197c81bcb2
Mike Sumner <Michael.Sumner@aad.gov.au>
2014-02-28 15:14:08 +1100

    using new testthat scheme

--- tests/test-all.R
+++ tests/test-all.R
@@ -1,6 +1,9 @@
 library(testthat)
-library(raadtools)
-fs <- list.files("tests", pattern = ".R$", full.names = TRUE)
+test_check("raadtools")
+
+##library(testthat)
+##library(raadtools)
+##fs <- list.files("tests", pattern = ".R$", full.names = TRUE)
 ##source(grep("chla", fs, value = TRUE)[1])
-for (i in seq_along(fs)) source(fs[i])
+##for (i in seq_along(fs)) source(fs[i])
 

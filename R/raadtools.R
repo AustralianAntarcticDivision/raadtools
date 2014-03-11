a58ee412a555d118963a90ca4882edd26c19ef43
Mike Sumner <Michael.Sumner@aad.gov.au>
2014-03-11 12:44:48 +1100

    removed useful AADC maps

--- R/raadtools.R
+++ R/raadtools.R
@@ -56,9 +56,6 @@ NULL
 ##' The ancient lost art of cartography.
 ##'
 ##' ibcso_background: The IBCSO RGB-map rasterlayer in high resolution
-##' ant_and_sthn_ocean_13989: Antarctica and the Sourthern Ocean
-##' ant_sthn_ocean_ed9_13939: Antarctica and the Sourthern Ocean Ed. 9
-##' kerguelen_to_antarctica_bathy_14033: Kerguelen and Antarctica
 ##' @title Maps of places
 ##' @param map name of the map to load
 ##' @param fact resize factor, see \code{\link[raster]{aggregate}}
@@ -66,19 +63,15 @@ NULL
 ##' @references
 ##' \url{http://www.ibcso.org/data.html}
 ##' @export
-imagemap <- function(map =
-                     c("ant_and_sthn_ocean_13989",
-                       "ant_sthn_ocean_ed9_13939",
-                       "ibcso_background_hq",
-                       "kerguelen_to_antarctica_bathy_14033"),
+imagemap <- function(map = c("ibcso_background_hq"),
                        fact = 1L) {
     datadir <- getOption("default.datadir")
     map <- match.arg(map)
     fpath <- switch(map,
-                    ibcso_background_hq = file.path(datadir,  "bathymetry", "ibcso", "image", "ibcso_background_hq.tif"),
-                    ant_and_sthn_ocean_13989 = file.path(datadir,  "maps", "ant_and_sthn_ocean_13989.tif"),
-                    ant_sthn_ocean_ed9_13939 = file.path(datadir, "maps", "ant_sthn_ocean_ed9_13939.tif"),
-                    kerguelen_to_antarctica_bathy_14033 = file.path(datadir, "maps",  "kerguelen_to_antarctica_bathy_14033.tif")
+                    ibcso_background_hq = file.path(datadir,  "bathymetry", "ibcso", "image", "ibcso_background_hq.tif")
+                   ## ant_and_sthn_ocean_13989 = file.path(datadir,  "maps", "ant_and_sthn_ocean_13989.tif"),
+                   ## ant_sthn_ocean_ed9_13939 = file.path(datadir, "maps", "ant_sthn_ocean_ed9_13939.tif"),
+                   ## kerguelen_to_antarctica_bathy_14033 = file.path(datadir, "maps",  "kerguelen_to_antarctica_bathy_14033.tif")
         )
 
     if (file.exists(fpath) & interactive()) message("\n\nremember to plot with plotRGB(x)")

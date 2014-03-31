799363c5918ef13e4ac549748a120c41deb57776
Mike Sumner <Michael.Sumner@aad.gov.au>
2014-04-01 10:52:28 +1100

    NEWS minor update

--- tests/test-all.R
+++ tests/test-all.R
@@ -4,6 +4,6 @@ library(testthat)
 #library(testthat)
 library(raadtools)
 fs <- list.files("testthat", pattern = ".R$", full.names = TRUE)
-source(grep("fileexists", fs, value = TRUE)[1])
-##for (i in seq_along(fs)) source(fs[i])
+##source(grep("fileexists", fs, value = TRUE)[1])
+for (i in seq_along(fs)) source(fs[i])
 

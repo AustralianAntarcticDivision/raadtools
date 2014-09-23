b7541c0f1ab17a4dba19675ffc2d3fe08a80d52d
Mike Sumner <mdsumner@gmail.com>
2014-09-24 00:46:25 +1000

    removed adminmap test

--- tests/testthat/testthat_admin.R
+++ tests/testthat/testthat_admin.R
@@ -1,12 +1,12 @@
-library(testthat)
-library(raadtools)
-
-test_that("all layers are available", {
-    expect_that(adminmap(), is_a("SpatialPolygonsDataFrame"))
-    expect_that(adminmap("sectors"), is_a("SpatialPolygonsDataFrame"))
-    expect_that(adminmap("habitatAssess"), is_a("SpatialPolygonsDataFrame"))
-    expect_that(adminmap("habitatAssessLL"), is_a("SpatialPolygonsDataFrame"))
-    expect_that(adminmap("habitatAssessFull"), is_a("SpatialPolygonsDataFrame"))
-    expect_that(adminmap("habitatAssessFullLL"), is_a("SpatialPolygonsDataFrame"))
-    expect_that(adminmap("a layer"), throws_error())
-})
+# library(testthat)
+# library(raadtools)
+# 
+# test_that("all layers are available", {
+#     expect_that(adminmap(), is_a("SpatialPolygonsDataFrame"))
+#     expect_that(adminmap("sectors"), is_a("SpatialPolygonsDataFrame"))
+#     expect_that(adminmap("habitatAssess"), is_a("SpatialPolygonsDataFrame"))
+#     expect_that(adminmap("habitatAssessLL"), is_a("SpatialPolygonsDataFrame"))
+#     expect_that(adminmap("habitatAssessFull"), is_a("SpatialPolygonsDataFrame"))
+#     expect_that(adminmap("habitatAssessFullLL"), is_a("SpatialPolygonsDataFrame"))
+#     expect_that(adminmap("a layer"), throws_error())
+# })

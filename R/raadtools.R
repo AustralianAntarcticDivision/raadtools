d880a1f4cde3fd278679ebbf10683e269c173883
Mike Sumner <mdsumner@gmail.com>
2014-12-01 20:19:03 -1000

    imports

--- R/raadtools.R
+++ R/raadtools.R
@@ -9,12 +9,13 @@
 ##' @name raadtools
 ##' @docType package
 ##' @keywords package
+##' @import methods
+##' @importFrom sp as.image.SpatialGridDataFrame bbox CRS GridTopology overlay proj4string<- SpatialPoints SpatialPointsDataFrame spChFIDs spTransform 
+##' @importFrom maptools ContourLines2SLDF spRbind
+##' @importFrom raster brick crop deratify extent<- extract getZ nlayers projection projection<- raster res resample rotate setZ stack writeRaster xmax xmin ymax ymin
+
 NULL
 
-
-#' @importFrom sp as.image.SpatialGridDataFrame bbox CRS GridTopology overlay proj4string<- SpatialPoints SpatialPointsDataFrame spChFIDs spTransform 
-#' @importFrom maptools ContourLines2SLDF spRbind
-#' @importFrom raster brick crop deratify extent<- extract getZ nlayers projection projection<- raster res resample rotate setZ stack writeRaster xmax xmin ymax ymin
 
 ## internal rotate to match old behaviour
 ## https://r-forge.r-project.org/scm/viewvc.php/pkg/raster/R/rotate.R?root=raster&r1=2782&r2=2981

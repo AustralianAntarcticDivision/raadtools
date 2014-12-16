4e77d219e57c178f4dc8a5ff9a706aebc304f262
Mike Sumner <mdsumner@gmail.com>
2014-12-15 17:17:20 -1000

    update to work with VRT for smith and sandwell

--- R/topo.R
+++ R/topo.R
@@ -1,3 +1,83 @@
+.ssbintext <- function() {
+  '<VRTDataset rasterXSize="21600" rasterYSize="17280">
+  <GeoTransform>0.0, 1855.325, 0.0000000000000000e+000, 15987768, 0.0000000000000000e+000,-1850.436</GeoTransform>
+  <SRS>PROJCS["unnamed",
+  GEOGCS["Normal Sphere (r=6370997)",
+  DATUM["unknown",
+  SPHEROID["sphere",6370997,0]],
+  PRIMEM["Greenwich",0],
+  UNIT["degree",0.0174532925199433]],
+  PROJECTION["Mercator_1SP"],
+  PARAMETER["central_meridian",0],
+  PARAMETER["scale_factor",1],
+  PARAMETER["false_easting",0],
+  PARAMETER["false_northing",0]]</SRS>
+  <Metadata />
+  <VRTRasterBand dataType="Int16" band="1" subClass="VRTRawRasterBand">
+  <Metadata />
+  <SourceFilename relativeToVRT="1">ssbinfile</SourceFilename>
+  <SourceBand>1</SourceBand>
+  <ImageOffset>0</ImageOffset>
+  <PixelOffset>2</PixelOffset>
+  <LineOffset>43200</LineOffset>
+  <ByteOrder>MSB</ByteOrder>
+  </VRTRasterBand>
+  </VRTDataset>'
+}
+
+.ssatlanticbintext <- function() {
+  '<VRTDataset rasterXSize="21600" rasterYSize="17280">
+  <SRS>PROJCS["unnamed",
+    GEOGCS["Normal Sphere (r=6370997)",
+  DATUM["unknown",
+  SPHEROID["sphere",6370997,0]],
+  PRIMEM["Greenwich",0],
+  UNIT["degree",0.0174532925199433]],
+  PROJECTION["Mercator_1SP"],
+  PARAMETER["central_meridian",0],
+  PARAMETER["scale_factor",1],
+  PARAMETER["false_easting",0],
+  PARAMETER["false_northing",0]]</SRS>
+  <GeoTransform>-20037510.000, 1.8553250000000000e+003, 0.0000000000000000e+000, 1.5987768000000000e+007, 0.0000000000000000e+000,-1.8504359999999999e+003</GeoTransform>
+  <Metadata />
+  <VRTRasterBand dataType="Int16" band="1">
+  <Metadata />
+  <SimpleSource>
+  <SourceFilename relativeToVRT="1">ssvrtfile</SourceFilename>
+  <SourceBand>1</SourceBand>
+  <SourceProperties RasterXSize="21600" RasterYSize="17280" DataType="Int16" BlockXSize="21600" BlockYSize="1" />
+  <SrcRect xOff="10800" yOff="0" xSize="10800" ySize="17280" />
+  <DstRect xOff="0" yOff="0" xSize="10800" ySize="17280" />
+  </SimpleSource>
+  <SimpleSource>
+  <SourceFilename relativeToVRT="1">ssvrtfile</SourceFilename>
+  <SourceBand>1</SourceBand>
+  <SourceProperties RasterXSize="21600" RasterYSize="17280" DataType="Int16" BlockXSize="21600" BlockYSize="1" />
+  <SrcRect xOff="0" yOff="0" xSize="10800" ySize="17280" />
+  <DstRect xOff="10800" yOff="0" xSize="10800" ySize="17280" />
+  </SimpleSource>
+  </VRTRasterBand>
+  </VRTDataset>'
+}
+
+
+.smithsandwellraw <- function(candidatefiles) {
+  tail(sort( grep("/topo_.*\\.img$", candidatefiles, value = TRUE)), 1)
+}
+
+.smithsandwellvrt <- function(lon180 = FALSE) {
+  binfile <- .smithsandwellraw(allfiles()$fullname)
+  vrtfile1 <- file.path(dirname(binfile), ".vrt", gsub(".img$", ".vrt", basename(binfile)))
+  vrtfile2 <- file.path(dirname(binfile), ".vrt",gsub(".img$", "atlantic.vrt", basename(binfile)))
+                        
+  vrtext1 <- gsub("ssbinfile", file.path("..", basename(binfile)), .ssbintext())
+  vrtext2 <- gsub("ssvrtfile", basename(vrtfile1), .ssatlanticbintext())
+  
+  if (!file.exists(vrtfile1)) writeLines(vrtext1, vrtfile1)
+  if (!file.exists(vrtfile2)) writeLines(vrtext2, vrtfile2)
+  if (lon180) vrtfile2 else vrtfile1
+}
+
 
 ##' @rdname readtopo
 ##' @export
@@ -20,6 +100,11 @@ topofile <- function(topo = c("gebco_08", "ibcso",
     }
   }
   if (!lon180 & !(topo %in% c("smith_sandwell"))) warning("no Pacific view version available of ", topo)
+  
+  if (topo == "smith_sandwell") {
+    topopath <- .smithsandwellvrt(lon180 = lon180)
+  } else {
+    
   topopath <- file.path(datadir, "bathymetry", topo,
                         switch(topo,
                                gebco_08 = "gebco_08.tif",
@@ -27,10 +112,11 @@ topofile <- function(topo = c("gebco_08", "ibcso",
                                etopo1 = "ETOPO1_Ice_g_gdal.grd",
                                etopo2 = "ETOPO2v2c_f4.nc",
                                kerguelen = "kerg_dem_100m.grd",
-                               george_v_terre_adelie = "gvdem100m_v3.nc",
+                               george_v_terre_adelie = "gvdem100m_v3.nc"
                                ## use the RAW file via GDAL VRT
-                               smith_sandwell = if (lon180) "topo_15.1_Atlantic.vrt" else "topo_15.1.vrt")
-  )
+                               
+  ))
+  }
   topopath
 }
 

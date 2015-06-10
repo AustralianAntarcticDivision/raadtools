39144942396536fb0575bcf11b12d1f28d0e1670
Michael Sumner <mdsumner@gmail.com>
2015-06-10 12:11:43 +1000

    Update ice.R
    turn off big stacker, which was failing to apply setNA/rescale

--- R/ice.R
+++ R/ice.R
@@ -173,7 +173,7 @@ readice <- function(date,
     x
   }
   
-  if (!product == "nsidc") {
+ ## if (!product == "nsidc") {
   ## loop over file indices
   for (ifile in seq_len(nfiles)) {
     r0 <- switch(product,
@@ -185,9 +185,9 @@ readice <- function(date,
     r[[ifile]] <- r0
   }
     r <- stack(r)
-  } else {
-    r <- stack(files$fullname)
-  }
+  ##} else {
+  ##  r <- stack(files$fullname)
+  ##}
   
   projection(r) <- prj
   names(r) <- basename(files$file)

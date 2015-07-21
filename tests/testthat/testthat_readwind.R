1ee98c0a364e95c22c5b73396c26609afef1fa0b
Michael Sumner <Michael.Sumner@aad.gov.au>
2015-07-21 14:28:36 +1000

    Stack always

--- tests/testthat/testthat_readwind.R
+++ tests/testthat/testthat_readwind.R
@@ -5,7 +5,7 @@ require(raadtools)
 
 test_that("read returns a Raster", {
 
-    expect_that(readwind(), is_a("RasterBrick"))
+    expect_that(readwind(), is_a("RasterStack"))
     expect_that(nlayers(readwind(magonly = TRUE)), equals(1L))
 
 })

b83e495751c8d7685fe907bff4f30b0ac35a9491
Michael Sumner <mdsumner@gmail.com>
2015-11-25 11:25:24 +1100

    Update ocfiles.R
    default .nc rather than .main

--- R/ocfiles.R
+++ R/ocfiles.R
@@ -15,6 +15,7 @@
                     varname = c("RRS", "CHL", "POC", "KD490", "PAR"), 
                     type = c("L3b", "L3m"),
                     bz2.rm = TRUE, 
+                    ext = c("nc", "main"), 
                     ...) {
   datadir <- getOption("default.datadir")
   ftx <- .allfilelist()
@@ -32,7 +33,7 @@
   #Note: ST92 is the test set designation for the SeaWiFS test run, similarly 
   #AT108 and AT109 are the MODIS-Aqua test set designators.  These designations 
   # will NOT be part of the reprocessing filenames.  
-  mtag <- sprintf("%s\\.main", paste(type, time, varname, sep = "_"))
+  mtag <- sprintf(paste0("%s\\.", ext), paste(type, time, varname, sep = "_"))
   
   ##cfiles1 <- sapply(product, function(x) file.path("oceandata.sci.gsfc.nasa.gov", x)
   cfiles1 <- grep(file.path("oceandata.sci.gsfc.nasa.gov", product), ftx, value = TRUE)

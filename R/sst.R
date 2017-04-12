b39a885dcc7af8974ecd898333071e8031f34442
Michael Sumner <mdsumner@gmail.com>
2017-04-12 16:29:53 +0000

    update readsst

--- R/sst.R
+++ R/sst.R
@@ -50,69 +50,15 @@ sstfiles <- function(time.resolution = c("daily","monthly"), ...) {
 }
 
 
-
-
-.sstfiles1 <- function (time.resolution = c("daily", "monthly"), fromCache = TRUE,
-                        ...)
-{
-  datadir <- getOption("default.datadir")
-  time.resolution <- match.arg(time.resolution)
-  if (fromCache) {
-    load(file.path(datadir, "cache", sprintf("sstfiles_%s.Rdata",
-                                             time.resolution)))
-    ## kludge fix
-    imatch <- which("files" == names(sstf))
-    if (length(imatch) > 0) names(sstf)[imatch] <- "file"
-    ###############
-    sstf$fullname <- file.path(datadir, sstf$file)
-    return(sstf)
-  }
-  if (time.resolution == "daily") {
-    dirpath <- file.path(datadir, "sst", "OI-daily-v2", "daily")
-    fs <- list.files(dirpath, pattern = "\\.nc$", recursive = TRUE,
-                     full.names = TRUE)
-    fsstrings <- as.Date(substr(basename(fs), 15, 22), "%Y%m%d")
-    dates <- timedateFrom(as.Date(fsstrings, "%Y%m%d", tz = "UTC"))
-    sstf <- data.frame(file = gsub("^/", "", gsub(datadir, "",
-                                                  fs)), date = dates, stringsAsFactors = FALSE)[order(dates),
-                                                                                                ]
-  }
-  if (time.resolution == "monthly") {
-    dirpath <- file.path(datadir, "sst", "oiv2")
-    r <- stack(file.path(dirpath, "sst.mnmean.nc"), quick = TRUE)
-
-    fs <- rep(file.path(dirpath, "sst.mnmean.nc"), nlayers(r))
-
-    dates <- timedateFrom(strptime(names(r), "X%Y.%m.%d"))
-    sstf <- data.frame(file = gsub("^/", "", gsub(datadir, "",
-                                                  fs)), date = dates, stringsAsFactors = FALSE)[order(dates),
-                                                                                                ]
-    sstf$band <- seq_len(nlayers(r))
-  }
-
-  save(sstf, file = file.path(datadir, "cache", sprintf("sstfiles_%s.Rdata",
-                                                        time.resolution)))
-  sstf
-}
-
-.progressreport <- function(current, finish) {
-  cat(paste(rep("\b", 16), collapse = ""))
-  cat(sprintf("%6d of %6d", current, finish));
-  flush.console()
-  invisible(NULL)
-}
-
-
-.readmonsst <- function(date) {
-  
-}
-##' Read OISST sea surface temperature data from daily files
+##' Read OISST sea surface temperature data
 ##'
 ##' SST data read from files managed by
 ##' \code{\link{sstfiles}}. Dates are matched to file names by finding
 ##' the nearest match in time within a short duration. If \code{date}
 ##' is greater than length 1 then the sorted set of unique matches is
 ##' returned.
+##' 
+##' 
 ##' @param date date or dates of data to read, see Details
 ##' @param time.resolution time resoution data to read, daily or monthly
 ##' @param xylim spatial extents to crop from source data, can be anything accepted by \code{\link[raster]{extent}}, see Details
@@ -152,61 +98,44 @@ readsst <-  function (date, time.resolution = c("daily", "monthly"),
   time.resolution <- match.arg(time.resolution)
   varname <- match.arg(varname)
   ## if (time.resolution == "monthly") stop("sorry, no monthly SST at the moment")
-
-  
   if (is.null(inputfiles)) {
     files <- sstfiles(time.resolution = time.resolution)
-    
   } else {
     files <- inputfiles
   }
-  
   if (returnfiles)
     return(files)
-
-
   if (missing(date)) date <- min(files$date)
   if (latest) date <- max(files$date)
   date <- timedateFrom(date)
   files <- .processFiles(date, files, time.resolution)
-
-  cropit <- FALSE
-  if (!is.null(xylim)) {
-    cropit <- TRUE
-    cropext <- extent(xylim)
-  }
   nfiles <- nrow(files)
-  
+  ## progress
+  pb <- progress::progress_bar$new(
+    format = "  extracting [:bar] :percent in :elapsed",
+    total = nfiles, clear = FALSE, width= 60)
+  pb$tick(0)
+  read_fun <- function(xfile, ext, msk, rot, varname = "", band = 1) {
+                   pb$tick()
+                      mask_if_needed(crop_if_needed(rotate_if_needed(raster(xfile, varname = varname, band = band), rot), ext), msk)
+                    }
+
+  ## TODO determine if we need to rotate, or just shift, or not anything
+  rot <- lon180
+  msk <- NULL
   if (time.resolution == "monthly") {
-    r0 <- suppressWarnings(stack(files$fullname[1], bands = files$band))
     if (setNA) {
-      mm <- raster(file.path(dirname(files$fullname[1]), "lsmask.nc"))
-      mm[mm < 1] <- NA_real_
+      msk <- crop_if_needed(rotate_if_needed(raster(file.path(dirname(files$fullname[1]), "lsmask.nc")), rot), xylim)
+      msk[msk < 1] <- NA_real_
     }
-  } else {
-    r0 <- suppressWarnings(stack(files$fullname, quick = TRUE, varname = varname))
-  }
- 
-  ## note that here the object gets turned into a brick,
-  ## presumably with a tempfile backing - that's something to think about more
-  ## in terms of passing in "filename"
-  if (lon180) {
-    r0 <- rotate(r0)
-    if (setNA & time.resolution == "monthly") mm <- rotate(mm)
-  }
-  if (cropit) {
-    r0 <- crop(r0, cropext, snap = "out")
-    if (setNA & time.resolution == "monthly") mm <- crop(mm, cropext, snap = "out")
-  }
-  if (setNA & time.resolution == "monthly") r0 <- mask(r0, mm)
+    #r0 <- stack(files$fullname[1], bands = files$band)
+  } 
+
+  if (is.null(files$band)) files$band <- 1
+  r0 <- brick(stack(lapply(seq_len(nrow(files)), function(xi) read_fun(files$fullname[xi], ext = xylim, msk = msk, rot = rot, varname = varname, band = files$band[xi]))), 
+              ...)
   if (is.na(projection(r0))) projection(r0) <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0"
   r0 <- setZ(r0, files$date)
-
-  ## need to determine if "filename" was passed in
-  dots <- list(...)
-  if ("filename" %in% names(dots)) {
-    r0 <- writeRaster(r0, ...)
-  }
 
   if (nfiles == 1) r0 <- r0[[1L]]
   r0
@@ -215,84 +144,21 @@ readsst <-  function (date, time.resolution = c("daily", "monthly"),
 
 
 
-
-
-
-.readsst1 <- function(date, time.resolution = c("daily", "monthly"), varname = c("sst", "anom", "err", "ice"),
-                      xylim = NULL,
-                      lon180 = TRUE,
-                      returnfiles = FALSE,
-                      verbose = TRUE,
-                      ...) {
-
-  datadir <- getOption("default.datadir")
-  time.resolution <- match.arg(time.resolution)
-  varname <- match.arg(varname)
-
-  if (! varname == "sst" & time.resolution == "monthly") stop("only sst available for monthly data")
-  files <- .sstfiles1(time.resolution = time.resolution)
-  if (returnfiles) return(files)
-
-  if (missing(date)) date <- min(files$date)
-  date <- timedateFrom(date)
-  ## from this point one, we don't care about the input "date" - this is our index into all files and that's what we use
-  ##findex <- .processDates(date, files$date, time.resolution)
-  ##date <- files$date[findex]
-  files <- .processFiles(date, files, time.resolution)
-
-  ## process xylim
-  cropit <- FALSE
-  if (!is.null(xylim)) {
-    cropit <- TRUE
-    cropext <- extent(xylim)
-    ##rtemplate <- crop(rtemplate, cropext)
-  }
-
-  nfiles <- nrow(files)
-  r <- vector("list", nfiles)
-  if (time.resolution == "daily") {
-    rtemplate <- raster(files$fullname[1L], varname = varname)
-    if (lon180) rtemplate <- .rotate(rtemplate)
-    for (ifile in seq_len(nfiles)) {
-      r0 <- raster(files$fullname[ifile], varname = varname)
-      if (lon180) r0 <- .rotate(r0)
-      if(cropit) r0 <- crop(r0, cropext)
-      r0[r0 < -2] <- NA
-      r[[ifile]] <- r0
-    }
-    ## monthly
+crop_if_needed <- function(x, ext = NULL) {
+  if (!is.null(ext)) {
+    crop(x, ext, snap = "out") 
   } else {
-    ##landmask <- readBin(file.path(datadir, "sst", "oimonth_v2", "lstags.onedeg.dat"), "numeric", size = 4, n = 360 * 180, endian = "big")
-    landmask <- raster(file.path(datadir, "sst", "oiv2", "lsmask.nc"))
-    for (ifile in seq_len(nfiles)) {
-      fname <- files$fullname[ifile]
-      ##con <- gzfile(fname, open = "rb")
-      ##version <- readBin(con, "integer", size = 4, n = 1, endian = "big")
-      ##date <-  readBin(con, "integer", size = 4, n = 7, endian = "big")
-      ##d <- readBin(con, "numeric", size = 4, n = 360 * 180, endian = "big")
-      ##close(con)
-      ## d[d > 500] <- NA
-      ##d[landmask < 1] <- NA
-      ##sst <- flip(raster(t(matrix(d,   360, 180)),
-      ##                   xmn = 0, xmx = 360, ymn = -90, ymx = 90,
-      ##                   crs = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0"), "y")
-      sst <- raster(fname, band = files$band[ifile])
-      sst[!landmask] <- NA
-      if (lon180) sst <- .rotate(sst)
-      if (cropit) sst <- crop(sst,cropext)
-      r[[ifile]] <- sst
-    }
-
-
-
+    x
   }
-
-  if (nfiles > 1) r <- brick(stack(r), ...) else r <- r[[1L]]
-  names(r) <- paste("sst", time.resolution, format(files$date, "%Y%m%d"), sep = "_")
-  r <- setZ(r, files$date)
-
-  return(r)
-
-
 }
+mask_if_needed <- function(x, msk = NULL) {
+  if (!is.null(msk)) {
+    x <- mask(x, msk)
+  } 
+  x
+}
+rotate_if_needed <- function(x, rot = FALSE) {
+  if (rot) .rotate(x) else x
+}
+
 

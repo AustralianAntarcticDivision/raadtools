74a60c526c71836c5e4ff6b4c67fc80048c19a57
Michael Sumner <mdsumner@gmail.com>
2017-06-07 00:56:41 +1000

    cleanup

--- R/zzz.R
+++ R/zzz.R
@@ -29,7 +29,7 @@
   ## try RAAD_DIR, which may only be available to R CMD check from ~/.R/check.Renviron
   r <- getOption("repos")
   dd <- getOption("default.datadir")
-   print(Sys.getenv("RAAD_DIR"))
+ #  print(Sys.getenv("RAAD_DIR"))
   if (is.null(dd["default.datadir"])) {
     dd["default.datadir"] <- Sys.getenv("RAAD_DIR");
     options(repos = r, default.datadir = dd); 

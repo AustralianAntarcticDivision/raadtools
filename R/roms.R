8be2558f73e9b03949e592e4181eabb6acb7a783
Michael Sumner <mdsumner@gmail.com>
2017-10-03 21:32:33 +1100

    speed up cpolarfiles, and apply 2009 epoch

--- R/roms.R
+++ R/roms.R
@@ -9,7 +9,15 @@
 #' @examples
 #' cpolarfiles()
 cpolarfiles <- function(...) {
+  dplyr::transmute(dplyr::filter(raadfiles:::get_raw_raad_filenames(), 
+                              grepl("s_corney/cpolar", file)),
+                fullname = file.path(root, file), 
+                date = as.POSIXct(strptime(sprintf("200%i-01", as.integer(substr(basename(fullname), 12, 14))+800), "%Y%m-%d"), tz = "GMT"))
+}
+
+
+cpolarfiles_old <- function(...) {
   dplyr::mutate(dplyr::filter(raadtools::allfiles(), 
-      grepl("s_corney/cpolar", fullname)), 
-      date = as.POSIXct(strptime(sprintf("197%s-01", substr(basename(fullname), 12, 14)), "%Y%m-%d")))
+                              grepl("s_corney/cpolar", fullname)), 
+                date = as.POSIXct(strptime(sprintf("197%s-01", substr(basename(fullname), 12, 14)), "%Y%m-%d")) + 29 * 365.25 * 24 * 3600)
 }

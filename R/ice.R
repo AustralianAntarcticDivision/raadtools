7b9a7128e70ff93b28a90895458f305ca3859ee0
Michael Sumner <mdsumner@gmail.com>
2017-10-05 12:07:45 +1100

    Update ice.R

--- R/ice.R
+++ R/ice.R
@@ -176,7 +176,7 @@ readice_monthly <- function(date,
 }
 
 
-read_ice_internal <- function(files, hemisphere, rescale, setNA, xylim,  ...) {
+read_ice_internal <- function(files, hemisphere, rescale, setNA, xylim = NULL,  ...) {
   ## check that files are available
   ## NSIDC projection and grid size for the Southern Hemisphere
   prj  <- "+proj=stere +lat_0=-90 +lat_ts=-70 +lon_0=0 +k=1 +x_0=0 +y_0=0 +a=6378273 +b=6356889.449 +units=m +no_defs " 

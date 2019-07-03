3fbcbbb38f32c4dcc118af9932c488c2aa3ae325
Michael Sumner <mdsumner@gmail.com>
2019-07-03 15:08:27 +1000

    dist to ice cleanups

--- tests/testthat/test-distance-ice.R
+++ tests/testthat/test-distance-ice.R
@@ -1,11 +1,11 @@
 context("distance-ice")
 
-## TODO: Rename context
-## TODO: Add more tests
-
 test_that("ice distance", {
-  distance_to_ice(threshold = 50) %>% expect_s4_class("RasterLayer")
-  distance_to_ice_edge(latest = TRUE) %>% expect_s4_class("RasterLayer")
+  s <- distance_to_ice(threshold = 50) %>% expect_s4_class("RasterLayer")
+  n <- distance_to_ice(threshold = 50, hemisphere = "north") %>% expect_s4_class("RasterLayer")
+  expect_true(dim(s) == c(332, 316, 1))
+  expect_true(dim(n) == c(448, 304, 1))
   
-  distance_to_ice(as.Date("2016-08-10"), threshold = 15, product = "nsidc") %>% expect_s4_class("RasterLayer")
+  distance_to_ice_edge() %>% expect_s4_class("RasterLayer")
+  distance_to_ice(as.Date("2016-08-10"), threshold = 15) %>% expect_s4_class("RasterLayer")
 })

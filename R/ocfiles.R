04345fc29a15858e70567b7cb791bc43059a772f
Ben Raymond <ben.raymond@aad.gov.au>
2023-03-14 10:39:37 +1100

    new ocfile naming

--- R/ocfiles.R
+++ R/ocfiles.R
@@ -27,33 +27,47 @@ ocfiles <- function(time.resolution = c("daily", "weekly", "monthly", "weekly32"
   varname <- varname[1L]
   type <- match.arg(type)
   
-  time <- switch(time.resolution, 
-                 daily = "DAY", 
-                 weekly = "8D", 
-                 monthly = "MO", 
+  time <- switch(time.resolution,
+                 daily = "DAY",
+                 weekly = "8D",
+                 monthly = "MO",
                  weekly32 = "R32")
-  
-  prod <- switch(product, 
-                 MODISA = "^A", SeaWiFS = "^S", VIIRS = "^V")
-  ## don't forget those ST93c files!  
-  ## see here: http://oceancolor.gsfc.nasa.gov/DOCS/FormatChange.html
-  #Note: ST92 is the test set designation for the SeaWiFS test run, similarly 
-  #AT108 and AT109 are the MODIS-Aqua test set designators.  These designations 
-  # will NOT be part of the reprocessing filenames.  
-  mtag <- sprintf(paste0("%s.*\\.", ext), paste(type, time, varname, sep = "_"))
-  #print(mtag)
-  ##cfiles1 <- sapply(product, function(x) file.path("oceandata.sci.gsfc.nasa.gov", x)
-  cfiles2 <- grep(mtag, ftx, value = TRUE)
-  cfiles3 <- cfiles2[grep(prod, basename(cfiles2))]
-  
-  cfiles <- if (bz2.rm)  grep(paste0(ext, "$"), cfiles3, value = TRUE) else cfiles3
-  tokens <- .filetok(basename(cfiles))
-  if (length(cfiles) < 1) stop("no files found for ", paste(product, varname, type, time.resolution, collapse = ", "))
-  dates <- as.POSIXct(strptime(paste0(tokens$year, tokens$jday), "%Y%j", tz = "GMT"))
-  cfs <- tibble::tibble(fullname = cfiles, 
-                    date = dates)[order(dates), ]
-  cfs
-  
+  ## NOTE file naming changed from October 2019 (date varied from data set to data set), see https://oceancolor.gsfc.nasa.gov/docs/filenaming-convention/
+  ## old name e.g. A2020366.L3b_DAY_RRS.nc becomes AQUA_MODIS.20201231.L3b.DAY.RRS.nc
+  ## - platform from abbreviation ("A") to full name ("AQUA_MODIS")
+  ## - YYYYDOY to YYYYMMDD
+  ## - separator between file name components is "." not "_"
+
+  ## first look for files using the new naming convention
+  prod <- switch(product, MODISA = "AQUA_MODIS", SeaWiFS = "SEASTAR_SEAWIFS_GAC", MODIST = "TERRA_MODIS", CZCS = "NIMBUS7_CZCS", VIIRS = "SNPP_VIIRS")
+  ## note that there is also JPSS1_VIIRS
+  mtag <- paste0(prod, ".*", paste(type, time, varname, sep = "\\."), ".*\\.", ext) ## separator is "." with new naming
+  cfiles3 <- grep(mtag, ftx, value = TRUE)
+  if (length(cfiles) > 0) {
+      ## I think this is irrelevant with the new files (no bz2s?) but leave it anyway
+      cfiles <- if (bz2.rm)  grep(paste0(ext, "$"), cfiles3, value = TRUE) else cfiles3
+      if (length(cfiles) < 1) stop("no files found for ", paste(product, varname, type, time.resolution, collapse = ", "))
+      dates <- sub("\\..+", "", sub("^[^\\.]+\\.", "", basename(cfiles))) ## discard before leading separator, and after second (keep date component)
+      dates <- as.POSIXct(strptime(dates, "%Y%m%d", tz = "GMT"))
+  } else {
+    ## did not find any new files, try old file naming
+      prod <- switch(product, MODISA = "^A", SeaWiFS = "^S", VIIRS = "^V")
+      ## don't forget those ST93c files!
+      ## see here: http://oceancolor.gsfc.nasa.gov/DOCS/FormatChange.html
+      ##   Note: ST92 is the test set designation for the SeaWiFS test run, similarly
+      ##   AT108 and AT109 are the MODIS-Aqua test set designators.  These designations
+      ##   will NOT be part of the reprocessing filenames.  
+      mtag <- sprintf(paste0("%s.*\\.", ext), paste(type, time, varname, sep = "_"))
+      ##print(mtag)
+      ##cfiles1 <- sapply(product, function(x) file.path("oceandata.sci.gsfc.nasa.gov", x)
+      cfiles2 <- grep(mtag, ftx, value = TRUE)
+      cfiles3 <- cfiles2[grep(prod, basename(cfiles2))]
+      cfiles <- if (bz2.rm)  grep(paste0(ext, "$"), cfiles3, value = TRUE) else cfiles3
+      if (length(cfiles) < 1) stop("no files found for ", paste(product, varname, type, time.resolution, collapse = ", "))
+      tokens <- .filetok(basename(cfiles))
+      dates <- as.POSIXct(strptime(paste0(tokens$year, tokens$jday), "%Y%j", tz = "GMT"))
+  }
+  tibble::tibble(fullname = cfiles, date = dates)[order(dates), ]
 }
 
 

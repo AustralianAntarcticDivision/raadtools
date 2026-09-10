c015b853859742d7d4e2f7f287962b771dbef897
Michael Sumner <mdsumner@gmail.com>
2026-09-10 12:26:10 +0000

    keep old and new test, CDR added a day

--- tests/testthat/testthat-timezone-modify.R
+++ tests/testthat/testthat-timezone-modify.R
@@ -5,17 +5,15 @@
 # curr <- currentsfiles()
 # amps <- raadtools::amps_d1files()
 # oc <- raadtools::ocfiles()
- #ssh <- raadtools::sshfiles()
 ffr <- function(x) unclass(min(x$date))
-#ref <- c(ffr(sst), ffr(ice), ffr(wind), ffr(curr), ffr(amps), ffr(oc), ffr(ssh))
-ref <- as.integer(c(368150400, 278208000, 283996800, 725846400, 1445774400, 1025654400, 
-         725846400))
-
+ref <- as.integer(c(368150400, 278208000, 283996800, 725846400, 1445774400, 1025654400,
+                    725846400, 278121600))
 ffw <- function(x) {x <- as.integer(c(x)); attributes(x) <- NULL; x}
 test_that("date time underlying values don't change", {
- # Use terra::time() in the timezone fixture test
   expect_equal(ffw(terra::time(readsst(latest = FALSE))), ref[1])
-  expect_equal(ffw(terra::time(readice(latest = FALSE))), ref[2])
+  ## readice() defaults to the CDR now, which starts a day before NSIDC-0051
+  expect_equal(ffw(terra::time(readice(latest = FALSE))), ref[8])
+  expect_equal(ffw(terra::time(readice(latest = FALSE, product = "nsidc"))), ref[2])
   ##expect_equal(ffw(terra::time(readwind(latest = FALSE))[1]), ref[3])
   expect_equal(ffw(terra::time(readcurr(latest = FALSE))[1]), ref[4])
   expect_equal(ffw(terra::time(readamps_d1wind(latest = FALSE))[1]), ref[5])

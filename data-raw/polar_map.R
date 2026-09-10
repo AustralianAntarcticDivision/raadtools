6b10b819030f0dc4d85fe48ec3c28aee09d8e9fa
Michael Sumner <mdsumner@gmail.com>
2026-09-10 07:34:04 +0000

    convert pmap to SpatVector

--- data-raw/polar_map.R
+++ data-raw/polar_map.R
@@ -1,9 +1,12 @@
-pmap <- rnaturalearth::ne_coastline(scale = 10)
-pmap <- pmap[coordinates(rgeos::gCentroid(pmap, byid = TRUE))[,2] < 0, ]
-files <- icefiles()
-files$size <- unlist(future_map(files$fullname, fs::file_size))
-
-bad_nsidc <- as.integer(as.Date(files$date[files$size < 40000]))
-
-pmap <- raadtools:::pmap
-usethis::use_data( pmap, bad_nsidc, internal = TRUE)
+# pmap <- rnaturalearth::ne_coastline(scale = 10)
+# pmap <- pmap[coordinates(rgeos::gCentroid(pmap, byid = TRUE))[,2] < 0, ]
+# files <- icefiles()
+# files$size <- unlist(future_map(files$fullname, fs::file_size))
+# 
+# bad_nsidc <- as.integer(as.Date(files$date[files$size < 40000]))
+# 
+# pmap <- raadtools:::pmap
+# usethis::use_data( pmap, bad_nsidc, internal = TRUE)
+# 
+# pmap <- terra::vect(pmap)
+# usethis::use_data(pmap, bad_nsidc, internal = TRUE, overwrite = T)
